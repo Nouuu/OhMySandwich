8cc8cc92e764159e8c365de30d12736e662f4787
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-18 20:02:34 +0200

    update unit type

--- models/UnitType.cs
+++ models/UnitType.cs
@@ -12,5 +12,5 @@ public class UnitType
     public static UnitType SimpleUnit { get { return new UnitType(" "); } }
     public static UnitType Gram { get { return new UnitType("g "); } }
     public static UnitType Liter { get { return new UnitType("l "); } }
-    public static UnitType Slice { get { return new UnitType(" tranche "); } }
+    public static UnitType Slice { get { return new UnitType(" tranche de "); } }
 }
