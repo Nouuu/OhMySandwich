86b7959c374d17fc43a47169086fdb721ecaf903
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-18 20:02:40 +0200

    add basket

---
+++ models/Basket.cs
@@ -0,0 +1,34 @@
+﻿namespace OhMySandwich.models;
+
+public class Basket
+{
+    private readonly List<Sandwich> _sandwichList;
+
+    public Basket()
+    {
+        _sandwichList = new List<Sandwich>();
+    }
+
+    public void AddSandwich(Sandwich sandwich)
+    {
+        _sandwichList.Add(sandwich);
+    }
+
+    public List<Sandwich> GetSandwichList()
+    {
+        return new List<Sandwich>(_sandwichList);
+    }
+
+    public Price GetBasketPrice()
+    {
+        if (_sandwichList.Count == 0)
+        {
+            return new Price("", 0);
+        }
+
+        return new Price(
+            "€", // TODO: Get currency
+            _sandwichList.Sum(sandwich => sandwich.Price.Value)
+        );
+    }
+}
