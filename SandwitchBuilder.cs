eedafde7b52a132b703f2a1a566a661a7f3a05e1
Swann HERRERA <swann@devloup.dev>
2022-05-19 21:20:35 +0200

    add Sandwitch builder

--- SandwitchBuilder.cs
+++ SandwitchBuilder.cs
@@ -4,27 +4,54 @@ class SandwitchBuilder
 {
     private readonly string? name;
     private readonly Price? price;
-    private readonly IngredientStack[] ingredients;
+    private readonly List<IngredientStack> ingredients;
+
+    public SandwitchBuilder()
+    {
+        name = null;
+        price = null;
+        ingredients = new List<IngredientStack>();
+    }
+    
+    public SandwitchBuilder(string? name, Price? price, List<IngredientStack> ingredientStacks)
+    {
+        this.name = name;
+        this.price = price;
+        ingredients = ingredientStacks;
+    }
 
     public Sandwich getResult()
     {
-        return new Sandwich(name, ingredients, price);
+        if (name == null || price == null)
+        {
+            throw new InvalidOperationException();
+        }
+    return new Sandwich(name, ingredients.ToArray(), price.Value);
     }
 
     public SandwitchBuilder setName(string newName)
     {
-        return new SandwitchBuilder(newName, ingredients, price);
+        return new SandwitchBuilder(newName, price, ingredients);
     }
 
     public SandwitchBuilder addIngredient(IngredientStack ingredientStack)
     {
-        // todo push the new ingredient
-        return new SandwitchBuilder(name , ingredients.Clone(), price);
+        var newIngredients = new List<IngredientStack>(ingredients) { ingredientStack };
+        return new SandwitchBuilder(name , price, newIngredients);
     }
-
-    public SandwitchBuilder changePrice(Price price)
+    public SandwitchBuilder addIngredient(Ingredient ingredient, int count)
     {
-        return new SandwitchBuilder(name, ingredients, price);
+        var newIngredients = new List<IngredientStack>(ingredients) { new(ingredient, count) };
+        return new SandwitchBuilder(name , price, newIngredients);
     }
 
+    public SandwitchBuilder setPrice(Price price)
+    {
+        return new SandwitchBuilder(name, price, ingredients);
+    }
+
+    public SandwitchBuilder setPrice(int price)
+    {
+        return new SandwitchBuilder(name, new Price("€" ,price), ingredients);
+    }
 }
