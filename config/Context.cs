285ce7f6cbfb8a568e89fd9af9bac61997ef0e73
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-21 12:50:41 +0200

    create context class + first injection

---
+++ config/Context.cs
@@ -0,0 +1,13 @@
+﻿using OhMySandwich.invoices;
+
+namespace OhMySandwich.config;
+
+public class Context
+{
+    private InvoiceGenerator? _singletonInvoiceGenerator;
+
+    public InvoiceGenerator GetInvoiceGenerator()
+    {
+        return _singletonInvoiceGenerator ??= new DefaultInvoiceGenerator();
+    }
+}
