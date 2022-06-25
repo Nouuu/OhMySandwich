8d95fe59dad0347c49cedd7c33c675c006ac2407
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-06-25 13:59:09 +0200

    test iterator

--- OhMySandwich.Test/utils/AlphabetIteratorShould.cs
+++ OhMySandwich.Test/utils/AlphabetIteratorShould.cs
@@ -1,6 +1,5 @@
 using OhMySandwich.Infrastructure.utils;
 using Xunit.Abstractions;
-using Xunit.Sdk;
 
 namespace OhMySandwich.Test.utils;
 
@@ -8,20 +7,38 @@ public class AlphabetIteratorShould
 {
     private readonly AlphabetIterator _iterator;
     ITestOutputHelper helper;
-    
+
 
     public AlphabetIteratorShould(ITestOutputHelper helper)
     {
         this.helper = helper;
         _iterator = new AlphabetIterator();
     }
-    
+
     [Fact]
-    public void return_a_list_of_letters_in_alphabet()
-    { for (var i = 0; i < 10000; i++)
+    public void return_a_single_letters_in_alphabet()
+    {
+        var letter = 'A';
+        for (var i = 0; i < 26; i++)
         {
-            // Test output helper
-            helper.WriteLine(_iterator.NextIteration());
+            Assert.Equal(letter.ToString(), _iterator.NextIteration());
+            letter++;
         }
+    }
+
+    [Fact]
+    public void return_double_letters_in_alphabet()
+    {
+        for (var i = 0; i < 26; i++)
+        {
+            _iterator.NextIteration();
+        }
+        Assert.Equal("AA",_iterator.NextIteration());
+        Assert.Equal("AB",_iterator.NextIteration());
+        for (var i = 0; i < 26; i++)
+        {
+            _iterator.NextIteration();
+        }
+        Assert.Equal("BC",_iterator.NextIteration());
     }
 }
